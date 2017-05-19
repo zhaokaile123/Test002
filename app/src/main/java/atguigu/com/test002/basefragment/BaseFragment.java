15e242360da74a16e9fba9a881707708f12f5aaa
zhaokaile123 <zhaokaile011206>
2017-05-19 23:55:55 +0800

    基类建出来了

---
+++ app/src/main/java/atguigu/com/test002/basefragment/BaseFragment.java
@@ -0,0 +1,47 @@
+package atguigu.com.test002.basefragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ASUS on 2017/5/19.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+
+    public Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return initView();
+
+    }
+
+    public abstract View initView() ;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    public void initData() {
+
+    }
+}
